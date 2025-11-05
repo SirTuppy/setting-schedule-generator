78cc9f8f4ced1437146554b28cc4d944ac152e90
Luke Sherlock <lukesherlock99@hotmail.com>
2025-11-05 17:39:20 -0600

    Add templateCoords configuration for climbing data

---
+++ config.js
@@ -0,0 +1,108 @@
+const templateCoords = {
+            'DSN': {
+                'routes': {
+                    tableTop: 775,
+                    rowHeight: 27,
+                    leftTable: {
+                        date: { x: 55, width: 42 },
+                        location: { x: 94, width: 122 },
+                        climbType: { x: 232, width: 67 },
+                        setters: { x: 315, width: 52 }
+                    },
+                    rightTable: {
+                        date: { x: 418, width: 42 },
+                        location: { x: 457, width: 122 },
+                        climbType: { x: 595, width: 67 },
+                        setters: { x: 678, width: 52 }
+                    }
+                },
+                'boulders': {
+                    tableTop: 775,
+                    rowHeight: 27,
+                    leftTable: {
+                        date: { x: 55, width: 42 },
+                        location: { x: 94, width: 122 },
+                        climbType: { x: 232, width: 67 },
+                        setters: { x: 315, width: 52 }
+                    },
+                    rightTable: {
+                        date: { x: 418, width: 42 },
+                        location: { x: 457, width: 122 },
+                        climbType: { x: 595, width: 67 },
+                        setters: { x: 678, width: 52 }
+                    }
+                }
+            },
+            'HIL': {
+                'boulders': {
+                    tableTop: 775,
+                    rowHeight: 27,
+                    leftTable: {
+                        date: { x: 55, width: 42 },
+                        location: { x: 94, width: 122 },
+                        climbType: { x: 232, width: 67 },
+                        setters: { x: 315, width: 52 }
+                    },
+                    rightTable: {
+                        date: { x: 418, width: 42 },
+                        location: { x: 457, width: 122 },
+                        climbType: { x: 595, width: 67 },
+                        setters: { x: 678, width: 52 }
+                    }
+                }
+            },
+            'DTN': {
+                'boulders': {
+                    tableTop: 775,
+                    rowHeight: 27,
+                    leftTable: {
+                        date: { x: 55, width: 42 },
+                        location: { x: 94, width: 122 },
+                        climbType: { x: 232, width: 67 },
+                        setters: { x: 315, width: 52 }
+                    },
+                    rightTable: {
+                        date: { x: 418, width: 42 },
+                        location: { x: 457, width: 122 },
+                        climbType: { x: 595, width: 67 },
+                        setters: { x: 678, width: 52 }
+                    }
+                }
+            },
+            'FTW': {
+                'boulders': {
+                    tableTop: 775,
+                    rowHeight: 27,
+                    leftTable: {
+                        date: { x: 55, width: 42 },
+                        location: { x: 94, width: 122 },
+                        climbType: { x: 232, width: 67 },
+                        setters: { x: 315, width: 52 }
+                    },
+                    rightTable: {
+                        date: { x: 418, width: 42 },
+                        location: { x: 457, width: 122 },
+                        climbType: { x: 595, width: 67 },
+                        setters: { x: 678, width: 52 }
+                    }
+                }
+            },
+            'GVN': {
+                'routes': {
+                    tableTop: 775,
+                    rowHeight: 27,
+                    leftTable: {
+                        date: { x: 55, width: 42 },
+                        location: { x: 94, width: 122 },
+                        climbType: { x: 232, width: 67 },
+                        setters: { x: 315, width: 52 }
+                    },
+                    rightTable: {
+                        date: { x: 418, width: 42 },
+                        location: { x: 457, width: 122 },
+                        climbType: { x: 595, width: 67 },
+                        setters: { x: 678, width: 52 }
+                    }
+                }
+            }
+        };
