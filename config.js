39055ad3cb995ffbbf0fe4404f86c13ecd775e68
Luke Sherlock <lukesherlock99@hotmail.com>
2025-11-05 19:53:04 -0600

    Refactor templateCoords structure for clarity
    Refactored templateCoords to separate routes and boulders for DSN, GVN, and PLN, and adjusted header and table parameters.

--- config.js
+++ config.js
@@ -1,109 +1,66 @@
 const templateCoords = {
-            'DSN': {
-                'routes': {
-                    header: { x: 750, y: 75 },
-                    tableTop: 775,
-                    rowHeight: 27,
-                    leftTable: {
-                        date: { x: 55, width: 42 },
-                        location: { x: 94, width: 122 },
-                        climbType: { x: 232, width: 67 },
-                        setters: { x: 315, width: 52 }
-                    },
-                    rightTable: {
-                        date: { x: 418, width: 42 },
-                        location: { x: 457, width: 122 },
-                        climbType: { x: 595, width: 67 },
-                        setters: { x: 678, width: 52 }
-                    }
-                },
-                'boulders': {
-                    tableTop: 775,
-                    rowHeight: 27,
-                    leftTable: {
-                        date: { x: 55, width: 42 },
-                        location: { x: 94, width: 122 },
-                        climbType: { x: 232, width: 67 },
-                        setters: { x: 315, width: 52 }
-                    },
-                    rightTable: {
-                        date: { x: 418, width: 42 },
-                        location: { x: 457, width: 122 },
-                        climbType: { x: 595, width: 67 },
-                        setters: { x: 678, width: 52 }
-                    }
-                }
-            },
-            'HIL': {
-                'boulders': {
-                    tableTop: 775,
-                    rowHeight: 27,
-                    leftTable: {
-                        date: { x: 55, width: 42 },
-                        location: { x: 94, width: 122 },
-                        climbType: { x: 232, width: 67 },
-                        setters: { x: 315, width: 52 }
-                    },
-                    rightTable: {
-                        date: { x: 418, width: 42 },
-                        location: { x: 457, width: 122 },
-                        climbType: { x: 595, width: 67 },
-                        setters: { x: 678, width: 52 }
-                    }
-                }
-            },
-            'DTN': {
-                'boulders': {
-                    tableTop: 775,
-                    rowHeight: 27,
-                    leftTable: {
-                        date: { x: 55, width: 42 },
-                        location: { x: 94, width: 122 },
-                        climbType: { x: 232, width: 67 },
-                        setters: { x: 315, width: 52 }
-                    },
-                    rightTable: {
-                        date: { x: 418, width: 42 },
-                        location: { x: 457, width: 122 },
-                        climbType: { x: 595, width: 67 },
-                        setters: { x: 678, width: 52 }
-                    }
-                }
-            },
-            'FTW': {
-                'boulders': {
-                    tableTop: 775,
-                    rowHeight: 27,
-                    leftTable: {
-                        date: { x: 55, width: 42 },
-                        location: { x: 94, width: 122 },
-                        climbType: { x: 232, width: 67 },
-                        setters: { x: 315, width: 52 }
-                    },
-                    rightTable: {
-                        date: { x: 418, width: 42 },
-                        location: { x: 457, width: 122 },
-                        climbType: { x: 595, width: 67 },
-                        setters: { x: 678, width: 52 }
-                    }
-                }
-            },
-            'GVN': {
-                'routes': {
-                    tableTop: 775,
-                    rowHeight: 27,
-                    leftTable: {
-                        date: { x: 55, width: 42 },
-                        location: { x: 94, width: 122 },
-                        climbType: { x: 232, width: 67 },
-                        setters: { x: 315, width: 52 }
-                    },
-                    rightTable: {
-                        date: { x: 418, width: 42 },
-                        location: { x: 457, width: 122 },
-                        climbType: { x: 595, width: 67 },
-                        setters: { x: 678, width: 52 }
-                    }
-                }
-            }
-        };
+    // DSN: Separate Routes and Boulders pages
+    'DSN': {
+        'routes': {
+            header: { x: 740, y: 115 },
+            tableTop: 775,
+            rowHeight: 27,
+            leftTable: { date: { x: 48, width: 42 }, location: { x: 94, width: 142 }, climbType: { x: 252, width: 67 }, setters: { x: 335, width: 52 } },
+            rightTable: { date: { x: 408, width: 42 }, location: { x: 457, width: 142 }, climbType: { x: 615, width: 67 }, setters: { x: 698, width: 52 } }
+        },
+        'boulders': {
+            header: { x: 740, y: 115 },
+            tableTop: 775,
+            rowHeight: 27,
+            leftTable: { date: { x: 48, width: 42 }, location: { x: 94, width: 142 }, climbType: { x: 252, width: 67 }, setters: { x: 335, width: 52 } },
+            rightTable: { date: { x: 408, width: 42 }, location: { x: 457, width: 142 }, climbType: { x: 615, width: 67 }, setters: { x: 698, width: 52 } }
+        }
+    },
+    // GVN & PLN: Combined single page for Routes and Boulders
+    'GVN': {
+        'combined': {
+            header: { x: 740, y: 115 },
+            tableTop: 775,
+            rowHeight: 27,
+            leftTable: { date: { x: 48, width: 42 }, location: { x: 100, width: 142 }, climbType: { x: 252, width: 67 }, setters: { x: 335, width: 52 } },
+            rightTable: { date: { x: 408, width: 42 }, location: { x: 463, width: 142 }, climbType: { x: 615, width: 67 }, setters: { x: 698, width: 52 } }
+        }
+    },
+    'PLN': {
+        'combined': { // Assumes PLN layout is similar to GVN, adjust as needed
+            header: { x: 740, y: 115 },
+            tableTop: 775,
+            rowHeight: 27,
+            leftTable: { date: { x: 48, width: 42 }, location: { x: 100, width: 142 }, climbType: { x: 252, width: 67 }, setters: { x: 335, width: 52 } },
+            rightTable: { date: { x: 408, width: 42 }, location: { x: 463, width: 142 }, climbType: { x: 615, width: 67 }, setters: { x: 698, width: 52 } }
+        }
+    },
+    // HIL, DTN, FTW: Boulders-only single page
+    'HIL': {
+        'boulders': {
+            header: { x: 740, y: 115 },
+            tableTop: 775,
+            rowHeight: 27,
+            leftTable: { date: { x: 48, width: 42 }, location: { x: 94, width: 142 }, climbType: { x: 252, width: 67 }, setters: { x: 335, width: 52 } },
+            rightTable: { date: { x: 408, width: 42 }, location: { x: 457, width: 142 }, climbType: { x: 615, width: 67 }, setters: { x: 698, width: 52 } }
+        }
+    },
+    'DTN': {
+        'boulders': {
+            header: { x: 740, y: 115 },
+            tableTop: 775,
+            rowHeight: 27,
+            leftTable: { date: { x: 48, width: 42 }, location: { x: 94, width: 142 }, climbType: { x: 252, width: 67 }, setters: { x: 335, width: 52 } },
+            rightTable: { date: { x: 408, width: 42 }, location: { x: 457, width: 142 }, climbType: { x: 615, width: 67 }, setters: { x: 698, width: 52 } }
+        }
+    },
+    'FTW': {
+        'boulders': {
+            header: { x: 740, y: 115 },
+            tableTop: 775,
+            rowHeight: 27,
+            leftTable: { date: { x: 48, width: 42 }, location: { x: 94, width: 142 }, climbType: { x: 252, width: 67 }, setters: { x: 335, width: 52 } },
+            rightTable: { date: { x: 408, width: 42 }, location: { x: 457, width: 142 }, climbType: { x: 615, width: 67 }, setters: { x: 698, width: 52 } }
+        }
+    }
+};
